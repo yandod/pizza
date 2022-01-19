151c1fb4664b7ca87573d2b7850b90a8c1c106f0
Yusuke Ando <yandod@gmail.com>
2022-01-19 16:16:21 +0900

    initial commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+#docker compose run node npx create-react-app --template typescript ts-app
+docker compose run --service-ports node npm start 
---
+++ ts-app/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div id="header" className="clearfix">
+        <div className="inner_container">
+            <h1 id="header_logo"><a href="/">HOW MANY BEER AND PIZZA?</a></h1>
+            <h3 id="header_about">人数を入れるとビールとピザの発注量を算出してくれるウェブサービスです。</h3>
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
