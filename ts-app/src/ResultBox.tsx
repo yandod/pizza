150fb5d72934f44a1c846d73ca65897e9ca40b34
Yusuke Ando <yandod@gmail.com>
2022-01-21 12:14:52 +0900

    migrate to defaultProps with interface

--- ts-app/src/ResultBox.tsx
+++ ts-app/src/ResultBox.tsx
@@ -1,31 +1,44 @@
 import React from 'react';
 import NumberFormat from 'react-number-format';
 
-class ResultBox extends React.Component {
-    render(): React.ReactNode {
-        var number = 10;
-        var beer = 8;
-        var pizza = 2;
-        var total = 20000;
-        var price = 1000;
-        var brand = 'domino';
-        var subtotal_beer = 3000;
-        var subtotal_pizza = 17000;
+interface ResultBoxProps {
+    number?: number;
+    brand?: "domino" | "pizzala" | "pizzahut";
+    beer?: number;
+    pizza?: number;
+    total?: number;
+    price?: number;
+    subtotal_beer?: number;
+    subtotal_pizza? :number;
+}
 
+class ResultBox extends React.Component<ResultBoxProps> {
+    static defaultProps: ResultBoxProps = {
+        number: 10,
+        brand: 'domino',
+        beer: 8,
+        pizza: 2,
+        total: 20000,
+        price: 2000,
+        subtotal_beer: 3000,
+        subtotal_pizza: 17000
+    }
+    
+    render(): React.ReactNode {
         return (
             <div id="result_area">
                 <h2 id="result_title">発注量</h2>
                 <dl className="summary clearfix">
                     <dt>人数</dt>
-                    <dd>{number}名</dd>
+                    <dd>{this.props.number}名</dd>
                     <dt>ビール(350ml缶)</dt>
-                    <dd>{beer}本 (約 <NumberFormat value={subtotal_beer} displayType={'text'} thousandSeparator={true}/>円)</dd>
-                    <dt>ピザ(Lサイズ) {brand}</dt>
-                    <dd>{pizza}枚 (約<NumberFormat value={subtotal_pizza} displayType={'text'} thousandSeparator={true}/>円)</dd>
+                    <dd>{this.props.beer}本 (約 <NumberFormat value={this.props.subtotal_beer} displayType={'text'} thousandSeparator={true}/>円)</dd>
+                    <dt>ピザ(Lサイズ) {this.props.brand}</dt>
+                    <dd>{this.props.pizza}枚 (約<NumberFormat value={this.props.subtotal_pizza} displayType={'text'} thousandSeparator={true}/>円)</dd>
                     <dt>合計金額</dt>
-                    <dd><NumberFormat value={total} displayType={'text'} thousandSeparator={true}/>円</dd>
+                    <dd><NumberFormat value={this.props.total} displayType={'text'} thousandSeparator={true}/>円</dd>
                     <dt>一人あたりの金額</dt>
-                    <dd><NumberFormat value={price} displayType={'text'} thousandSeparator={true}/>円</dd>
+                    <dd><NumberFormat value={this.props.price} displayType={'text'} thousandSeparator={true}/>円</dd>
                 </dl>
                 <p id="banner_box">
                     <a href="https://click.linksynergy.com/fs-bin/click?id=NUwEQ5D8uZg&offerid=761398.13&type=4&subid=0"><img alt="ピザハットオンライン" src="https://static.pizzahut.jp/jp/content/234_60.jpg"/></a>
